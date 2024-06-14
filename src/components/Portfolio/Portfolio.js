fc03aab4b4ec9259d96babe152b239ab2c12ac8a
Shaqeel Less <54390756+MogammadShaqeelless16@users.noreply.github.com>
2024-06-14 07:48:40 +0000

    update

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -2,15 +2,6 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faPlus,
-    faTimes,
-    faHeart,
-    faCode,
-    faBook,
-    faHiking,
-    faGamepad,
-    faMusic,
-    faFutbol,
-    faLightbulb,
     faExternalLinkAlt,
     faEdit,
     faTrashAlt,
